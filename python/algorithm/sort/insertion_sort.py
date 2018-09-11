8dbc0a391407c558366ebb6f0f93bcb8466ad0e7
zhangws <zhangwenshan@hunting4x.com>
2018-09-11 22:30:22 +0800

    排序算法

--- python/algorithm/sort/insertion_sort.py
+++ python/algorithm/sort/insertion_sort.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
+import tools
+from tools import exectime
+
+
+@exectime
 def insert_sort(array):
     for i in range(len(array)):
         for j in range(i):
@@ -12,8 +17,18 @@ def insert_sort(array):
 
 
 def main():
+    """
+   ﻿（1）从第一个元素开始，该元素可以认为已经被排序；
+   （2）取出下一个元素，在已经排序的元素序列中从后向前扫描；
+   （3）如果该元素（已排序）大于新元素，将该元素移到下一位置；
+   （4）重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+   （5）将新元素插入到该位置后；
+   （6）重复步骤2~5。
+    """
 
-    print "插入排序"
+    # tools.dump_random_array()
+    array = tools.load_random_array()
+    print(insert_sort(array) == sorted(array))
 
 
 if __name__ == '__main__':
