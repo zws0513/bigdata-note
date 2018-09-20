020682b525cb1738b8b7988a1e01ad92471a6856
zhangws <zhangwenshan@gitv.cn>
2018-09-20 20:16:03 +0800

    解析快速排序算法

--- python/algorithm/sort/quick_sort.py
+++ python/algorithm/sort/quick_sort.py
@@ -8,7 +8,7 @@ import tools
 @tools.exectime
 def quick_sort(array):
     def recursive(begin, end):
-        if begin > end:
+        if begin >= end:
             return
         l, r = begin, end
         pivot = array[l]
@@ -19,11 +19,43 @@ def quick_sort(array):
                 l += 1
             array[l], array[r] = array[r], array[l]
 
+        # array[begin]存的是pivot，已经无用，而当前array[l]的位置就是pivot应该在的位置，把array[l]的值移到pivot的左边
         array[l], array[begin] = pivot, array[l]
         recursive(begin, l - 1)
         recursive(r + 1, end)
 
     recursive(0, len(array) - 1)
+    return array
+
+
+@tools.exectime
+def quick_sort2(array):
+    def recursive2(begin, end):
+        if begin >= end:
+            return
+
+        l, r = begin, end
+        key = array[l]
+
+        # 循环判断直到遍历全部
+        while l < r:
+            # 从右边开始查找大于参考点的值
+            while l < r and array[r] >= key:
+                r -= 1
+            array[l] = array[r]  # 这个位置的值先挪到左边
+
+            # 从左边开始查找小于参考点的值
+            while l < r and array[l] <= key:
+                l += 1
+            array[r] = array[l]  # 这个位置的值挪到右边
+
+        # 写回改成的值
+        array[l] = key
+
+        # 递归，并返回结果
+        recursive2(begin, l - 1)    # 递归左边部分
+        recursive2(l + 1, end)   # 递归右边部分
+    recursive2(0, len(array) - 1)
     return array
 
 
@@ -39,7 +71,8 @@ def main():
     """
 
     array = tools.load_random_array()
-    print quick_sort(array) == sorted(array)
+    # print quick_sort(array) == sorted(array)
+    print quick_sort2(array) == sorted(array)
 
 
 if __name__ == '__main__':
